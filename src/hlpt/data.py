50db03c92fb5eabd53027c796ecd48c06023e976
Darin Chau <darinchauyf@gmail.com>
2023-07-21 07:28:18 +0800

    tqdm

--- src/hlpt/data.py
+++ src/hlpt/data.py
@@ -18,44 +18,18 @@ def get_size(x):
         return 1
 
 class DataIterator(DataLoader):
-    """A wrapper for datasets and dataloaders that automatically perform train-test splits and stuff like a torch dataloader
+    """A wrapper for datasets that provides lovely progress bars
     
     label: if not None, we will perform train-test split in a stratified manner"""
-    def __init__(self, *t : Tensor, batch_size: int = 1, label: list | None = None, shuffle = True, progress_bar = True, use_multiprocess = True):
-        assert len(t) > 0
-        self.len_t = len(t[0])
-        for tensor in t:
-            assert len(tensor) == self.len_t
-        
-        super().__init__(TensorDataset(*t), batch_size=batch_size, shuffle = shuffle, num_workers=1 if use_multiprocess else 0)
-        self.label = label
+    def __init__(self, d: Dataset, batch_size: int = 1, shuffle = True, progress_bar = True, use_multiprocess = True):
+        super().__init__(d, batch_size=batch_size, shuffle = shuffle, num_workers = 1 if use_multiprocess else 0)
         self.pbar = progress_bar
         self.shuffle = shuffle
         self.use_multiprocess = use_multiprocess
         self.bs = batch_size
-    
-    def split(self, train_size = 0.85, *, seed = None) -> tuple[DataIterator, DataIterator]:
-        """Performs train-test split"""
-        trains = []
-        tests = []
-
-        if seed is None:
-            seed = 42069
-
-        for tensor in self.dataset[:]:
-            train, test = train_test_split(tensor.detach().cpu().numpy(), test_size = 1 - train_size, random_state=seed, shuffle = False, stratify=self.label)
-            train = torch.as_tensor(train, dtype = tensor.dtype, device = tensor.device)
-            test = torch.as_tensor(test, dtype = tensor.dtype, device = tensor.device)
-            trains.append(train)
-            tests.append(test)
-        
-        # Create new TensorDatasets and DataLoaders for train and test sets
-        train_loader = DataIterator(*trains, batch_size=self.bs, label=self.label, shuffle=self.shuffle, progress_bar=self.pbar, use_multiprocess=self.use_multiprocess)
-        test_loader = DataIterator(*tests, batch_size=self.bs, label=self.label, shuffle=self.shuffle, progress_bar=self.pbar, use_multiprocess=self.use_multiprocess)
-        return train_loader, test_loader
 
     def __iter__(self):
-        p = ProgressBar(total = self.len_t, disable = not self.pbar)
+        p = self.get_pbar()
         for x in super().__iter__():
             yield x
             p.update(get_size(x))
@@ -64,10 +38,17 @@ class DataIterator(DataLoader):
     def __add__(self, other: DataLoader):
         if not isinstance(other, DataLoader):
             raise RuntimeError("Cannot add data iterators with other things")
+        tmp = self.pbar
         self.pbar = False
         if isinstance(other, (DataIterator, ConcatIterator)):
             other.pbar = False
-        return ConcatIterator(self, other, self.batch_size, shuffle = self.shuffle, progress_bar = self.pbar, use_multiprocess = self.use_multiprocess)
+        return ConcatIterator(self, other, self.batch_size, shuffle = self.shuffle, progress_bar = tmp, use_multiprocess = self.use_multiprocess)
+
+    def get_pbar(self):
+        try:
+            return ProgressBar(total = len(self.dataset), disable = not self.pbar)
+        except AttributeError:
+            return ProgressBar(disable = not self.pbar)
 
 class ConcatIterator(DataLoader):
     def __init__(self, d1: DataLoader, d2: DataLoader, batch_size: int = 1, shuffle = True, progress_bar = True, use_multiprocess = True):
