98d3b90eb6013e039a19ba403ef294be54602a48
Darin Chau <darinchauyf@gmail.com>
2024-03-26 13:52:24 +0800

    Modify setup to fix installation problem

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-if __name__ == "__main__":
-    setup()
+VERSION = "0.1.0"
+
+setup(
+    name="mlutils",
+    version=VERSION,
+    description="A collection of utilities for deep learning models",
+    license_files=["LICENSE"],
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    keywords="deep learning",
+    license="MIT",
+    author="Darin Chau",
+    author_email="darinchauyf@gmail.com",
+    url="https://github.com/darinchau/mlutils",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+)
